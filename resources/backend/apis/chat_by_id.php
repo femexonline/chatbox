6bc090d137b908fd334888efa4c3b11de476dfd6
Femi Oloruntade <femioloruntade@gmail.com>
2025-03-13 00:19:42 +0100

    update

--- resources/backend/apis/chat_by_id.php
+++ resources/backend/apis/chat_by_id.php
@@ -6,7 +6,7 @@
         require_once $ROOT_DIR.'/resources/backend/controllers/UserController.php';
         require_once $ROOT_DIR.'/resources/backend/controllers/MessageController.php';
 
-        // yes
+        // yesrji
 
         $res=[
             "isErr"=>false,
