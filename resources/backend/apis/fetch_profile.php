caeb733e6a3ad510bad4239bfc0ea7d1ed41567d
Femi Oloruntade <femioloruntade@gmail.com>
2025-03-23 13:58:41 +0100

    update

--- resources/backend/apis/fetch_profile.php
+++ resources/backend/apis/fetch_profile.php
@@ -1,6 +1,8 @@
 <?php
     require_once '../root_path.php';
 
+    // just busy
+
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         require_once $ROOT_DIR.'/resources/backend/controllers/UserController.php';
 
