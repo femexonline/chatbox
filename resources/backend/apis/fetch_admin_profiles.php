cafc38a1f930ac6390a42e7cde4c5c11f5a76742
Femi Oloruntade <femioloruntade@gmail.com>
2025-03-27 11:38:14 +0100

    update

--- resources/backend/apis/fetch_admin_profiles.php
+++ resources/backend/apis/fetch_admin_profiles.php
@@ -2,6 +2,14 @@
     require_once '../root_path.php';
 
     require_once $ROOT_DIR.'/resources/backend/controllers/UserController.php';
+
+    // just busy
+    // just busy
+    // just busy
+    
+    // just busy
+    // just busy
+    // just busy
     
 
     $admins=UserController::getAdminsMax2();
