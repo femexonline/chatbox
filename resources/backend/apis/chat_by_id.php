18ae9ed0219f14b70ea373cc5a17df3a250454d2
Femi Oloruntade <femioloruntade@gmail.com>
2025-06-30 10:05:21 +0100

    update

--- resources/backend/apis/chat_by_id.php
+++ resources/backend/apis/chat_by_id.php
@@ -3,6 +3,9 @@
     // require_once '../root_path.php';
     // require_once '../root_path.php';
     // require_once '../root_path.php';
+    // require_once '../root_path.php';
+    // require_once '../root_path.php';
+    // require_once '../root_path.php';
 
     
     if($_SERVER["REQUEST_METHOD"]=="POST"){
