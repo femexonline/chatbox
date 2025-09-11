515c14d342332dcdb8139aa7fc5a9bbe8e91d703
Femi Oloruntade <femioloruntade@gmail.com>
2025-09-11 06:57:10 +0100

    update

--- resources/backend/apis/chat_by_id.php
+++ resources/backend/apis/chat_by_id.php
@@ -4,6 +4,7 @@
     // require_once '../root_path.php';
     // require_once '../root_path.php';
 
+
     if($_SERVER["REQUEST_METHOD"]=="POST"){
         require_once $ROOT_DIR.'/resources/backend/controllers/ChatController.php';
         require_once $ROOT_DIR.'/resources/backend/controllers/UserController.php';
