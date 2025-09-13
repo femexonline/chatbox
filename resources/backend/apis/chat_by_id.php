b553bd959acf0d5c50289d19275bda9483930164
Femi Oloruntade <femioloruntade@gmail.com>
2025-09-13 14:26:16 +0100

    upating design work on userNoAdmin

--- resources/backend/apis/chat_by_id.php
+++ resources/backend/apis/chat_by_id.php
@@ -2,6 +2,8 @@
     require_once '../root_path.php';
     // require_once '../root_path.php';
     // require_once '../root_path.php';
+    // require_once '../root_path.php';
+    // require_once '../root_path.php';
 
 
     if($_SERVER["REQUEST_METHOD"]=="POST"){
