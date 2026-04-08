9f764b0c65382868ad4217883f13467aeeb67144
Femi Oloruntade <femioloruntade@gmail.com>
2026-04-08 22:07:28 +0100

    update

--- resources/backend/api_socket2/_access_control.php
+++ resources/backend/api_socket2/_access_control.php
@@ -1,7 +1,6 @@
 <?php  
     // header("Access-Control-Allow-Origin: http://femex2.tk");
     // header("Access-Control-Allow-Origin: http://femex2.tk");
-    // header("Access-Control-Allow-Origin: http://femex2.tk");
     header("Access-Control-Allow-Methods: GET,POST,OPTIONS,DELETE,PUT");
     // header("Access-Control-Allow-Headers: origin, x-requested-with, content-type, access-control-allow-origin");
 ?>
